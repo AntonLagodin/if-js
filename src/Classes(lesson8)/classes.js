a8411b8ba95777c93b53efef2d86b9b205dca9c6
Anton Lagodin <lagodinanton@gmail.com>
2022-06-09 19:49:40 +0300

    lesson8 done

---
+++ src/Classes(lesson8)/classes.js
@@ -0,0 +1,66 @@
+export const studentsData = [
+  {
+    firstName: 'Василий',
+    lastName: 'Петров',
+    admissionYear: 2019,
+    courseName: 'Java',
+  },
+  {
+    firstName: 'Иван',
+    lastName: 'Иванов',
+    admissionYear: 2018,
+    courseName: 'JavaScript',
+  },
+  {
+    firstName: 'Александр',
+    lastName: 'Федоров',
+    admissionYear: 2017,
+    courseName: 'Python',
+  },
+  {
+    firstName: 'Николай',
+    lastName: 'Петров',
+    admissionYear: 2019,
+    courseName: 'Android',
+  },
+];
+export class User {
+  constructor(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+  get fullName() {
+    return `${this.firstName}  ${this.lastName}`;
+  }
+}
+
+export class Student extends User {
+  constructor(firstName, lastName, admissionYear, courseName) {
+    super(firstName, lastName);
+    this.admissionYear = admissionYear;
+    this.courseName = courseName;
+  }
+  get course() {
+    return new Date().getFullYear() - this.admissionYear;
+  }
+}
+
+export class Students {
+  constructor(studentsData) {
+    this.students = studentsData;
+  }
+
+  sortByYear(year) {
+    return (a, b) => (a[year] < b[year] ? 1 : -1);
+  }
+
+  get getInfo() {
+    this.students.sort(this.sortByYear('admissionYear'));
+    return this.students.map(
+      (student) =>
+        `${student.firstName} ${student.lastName} - ${student.courseName} ${
+          new Date().getFullYear() - student.admissionYear
+        } курс`,
+    );
+  }
+}
